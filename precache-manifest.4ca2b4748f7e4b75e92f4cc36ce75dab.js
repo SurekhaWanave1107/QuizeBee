62487f8a8cd4793b04acad7f0017ad93f8d03113
Surekha Wanave <surekha.wanave11@gmail.com>
2020-09-02 18:09:44 +0530

    initial commit

---
+++ precache-manifest.4ca2b4748f7e4b75e92f4cc36ce75dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe5e8b15bcc6e510f3e454456fd11c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75f3c8045b51b5b937e4",
+    "url": "/static/css/main.48c15748.chunk.css"
+  },
+  {
+    "revision": "4a47882ed700f77a7bee",
+    "url": "/static/js/2.89458eb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.89458eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f3c8045b51b5b937e4",
+    "url": "/static/js/main.a0754d74.chunk.js"
+  },
+  {
+    "revision": "ddab1a47e66a79c6fc8c",
+    "url": "/static/js/runtime-main.6394fe66.js"
+  }
+]);
